82425299c8571ec9364ff22dcb0c91ccc830296a
gonzalo.garcia <gonzalo.garcia@ascentis.com>
2022-03-21 12:15:44 -0700

    7.Including Identity Claim in an Access Token

--- ImageGallery/src/Marvin.IDP/Config.cs
+++ ImageGallery/src/Marvin.IDP/Config.cs
@@ -25,7 +25,10 @@ namespace Marvin.IDP
         public static IEnumerable<ApiResource> Apis =>
             new ApiResource[] 
             { 
-                new ApiResource("imagegalleryapi", "Image Gallery API")
+                new ApiResource(
+                    "imagegalleryapi",
+                    "Image Gallery API",
+                    new List<string>() { "role" })
             };
         
         public static IEnumerable<Client> Clients =>
